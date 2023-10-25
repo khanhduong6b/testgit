d514e0219782a04b7ff38fbc72da428e20c910de
khanduong6b <khanhduong6b@gmail.com>
2023-10-25 10:59:36 +0700

    Tong

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+	return a+b;
+};
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int a,b;
+	cout<<"\nNhap a: ";
+	cin>>a;
+	cout<<"\nNhap b: ";
+	cin>>b;
+	cout<<"\nTong a+b la: "a+b;
 	system("pause");
 	return 0;
 }
