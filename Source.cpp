d7593932b60a2ac069a4ce8d0c3d9cd3e1cd2b96
Trung <chautrung0402@gmail.com>
2023-10-25 10:50:35 +0700

    tinh hieu

--- Source.cpp
+++ Source.cpp
@@ -2,11 +2,16 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	return a-b;
+};
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	//tinh hieu
+	hieu=a-b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	system("pause");
 	return 0;
