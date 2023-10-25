2cab1cf681de2a10bcbb9e6eef931b24645c369e
khanduong6b <khanhduong6b@gmail.com>
2023-10-25 11:43:58 +0700

    Tong + Hieu + Tich

--- Source.cpp
+++ Source.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
-<<<<<<< HEAD
-int hieu(int a, int b);
-int tich(int a, int b){
-return a*b;
-}
-float thuong(int a, int b);
-int main()
-{
-int a,b;
-=======
+int tong(int a, int b){
+	return a+b;
+};
 int hieu(int a, int b)
 {
 	return a-b;
 };
-int tich(int a, int b);
+int tich(int a, int b){
+	return a*b;
+};
 float thuong(int a, int b);
 int main()
 {
-	//tinh hieu
-	hieu=a-b;
->>>>>>> refs/remotes/origin/main
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-cout<<"Nhap so a va b" <<endl;
-cin>>a>>b;
-cout<<"tich cua a va b la "<<tich(a,b)<<endl;
+	int a,b;
+	cout<<"\nNhap a: ";
+	cin>>a;
+	cout<<"\nNhap b: ";
+	cin>>b;
+	cout<<"\nTong a+b la: "tong(a,b);
+	cout<<"\nHieu la: "hieu(a,b);
+	cout<<"\nTich cua a va b la "<<tich(a,b)<<endl;
 	system("pause");
 	return 0;
 }
